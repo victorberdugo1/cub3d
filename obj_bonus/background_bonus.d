65d3df6de575b4340317ad4bfa4fef36a72c5d45
Victor <victor@localhost.localdomain>
2025-04-25 22:14:01 +0200

    HUD normi

--- obj_bonus/background_bonus.d
+++
@@ -1,5 +0,0 @@
-obj_bonus/background_bonus.o: src/bonus/background_bonus.c \
- inc/bonus/cub3D_bonus.h libft/libft.h MLX42/include/MLX42/MLX42.h
-inc/bonus/cub3D_bonus.h:
-libft/libft.h:
-MLX42/include/MLX42/MLX42.h:
